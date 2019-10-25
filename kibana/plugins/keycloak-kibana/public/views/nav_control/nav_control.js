1bc18125826bf50b112e80ffc4fac4ef30a4434f
roniez <ronny.nordstrand@gmail.com>
2019-10-25 09:42:23 +0200

    Update nav_control.js

--- kibana/plugins/keycloak-kibana/public/views/nav_control/nav_control.js
+++ kibana/plugins/keycloak-kibana/public/views/nav_control/nav_control.js
@@ -1,10 +1,10 @@
 /* eslint-disable import/no-unresolved */
-import { chromeHeaderNavControlsRegistry } from 'ui/registry/chrome_header_nav_controls';
+import { chromeHeaderNavControlsRegistry, NavControlSide } from 'ui/public/registry/chrome_header_nav_controls';
 import { I18nContext } from 'ui/i18n';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { NavControlSide } from 'ui/chrome/directives/header_global_nav';
+/* import { NavControlSide } from 'ui/chrome/directives/header_global_nav'; */
 import UserNavControl from '../../authz/components/UserNavControl';
 
 chromeHeaderNavControlsRegistry.register((principalProvider) => ({
